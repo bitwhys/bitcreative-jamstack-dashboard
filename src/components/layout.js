739160128764a98413303c00b99bce8cb4da8107
George Bockari <bitwhys@gmail.com>
2019-11-03 01:09:06 -0800

    refactor(layout.js): removing netlify identity component to test deploy

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
-import { IdentityContextProvider } from "react-netlify-identity-widget"
+// import { IdentityContextProvider } from "react-netlify-identity-widget"
 
 import "./layout.css"
+// <IdentityContextProvider url="https://bitcreative-jam.netlify.com">
+//
+// </IdentityContextProvider>
 
 const Layout = ({ children }) => (
-  <IdentityContextProvider url="https://bitcreative-jam.netlify.com">
+  <>
     <header>
       <Link to="/">JAMstack App</Link>
     </header>
     <main>{children}</main>
-  </IdentityContextProvider>
+  </>
 )
 
 export default Layout
